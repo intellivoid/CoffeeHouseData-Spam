064a346a87566427789cc392ef7addf8a7026772
Netkas <netkas@intellivoid.info>
2021-02-19 18:07:29 -0500

    Minor correction

--- normalize.py
+++ normalize.py
@@ -2,25 +2,31 @@ import os, time
 
 current_time = 0
 
+def process_change():
+    with open("input.txt") as fp:
+        line = fp.readline()
+        category = "Unknown"
+        data = ""
+        cnt = 1
+        while line:
+            if  cnt == 1:
+                category = line.lower().replace('\n', '')
+            else:
+                data += line + "\n" 
+            line = fp.readline()
+            cnt += 1
+        data = repr(data)[:-3][1:]
+        while "  " in data:
+            data = data.replace("  ", " ")
+        file_target = "chatrooms/{0}.dat".format(category)
+        print("Writing to {0}".format(file_target))
+        print("Content: {0}".format(data))
+        with open(file_target, "a") as target_file_stream:
+            target_file_stream.write(data + "\n")
+
 while True:
     current_stat = os.stat("input.txt")[8]
     if current_stat > current_time:
         current_time = current_stat
-        print("Change Detect")
-        with open("input.txt") as fp:
-            line = fp.readline()
-            category = "Unknown"
-            data = ""
-            cnt = 1
-            while line:
-                if  cnt == 1:
-                    category = line.lower().replace('\n', '')
-                else:
-                    data += line + "\n" 
-                line = fp.readline()
-                cnt += 1
-            data = repr(data)[:-3][1:]
-            while "  " in data:
-                data = data.replace("  ", " ")
-            with open("chatrooms/{0}.dat".format(category), "a") as myfile:
-                myfile.write(data + "\n")
+        print("Change Detected {0}".format(current_stat))
+        process_change()
