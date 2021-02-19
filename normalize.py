f9e92d5e325af79a15a75d75d3237ce8fa65abf1
Netkas <netkas@intellivoid.info>
2021-02-19 17:58:17 -0500

    Added auto change-detection

--- normalize.py
+++ normalize.py
@@ -1,19 +1,26 @@
-import os
+import os, time
 
-with open("input.txt") as fp:
-    line = fp.readline()
-    category = "Unknown"
-    data = ""
-    cnt = 1
-    while line:
-        if  cnt == 1:
-            category = line.lower().replace('\n', '')
-        else:
-            data += line + "\n" 
-        line = fp.readline()
-        cnt += 1
-    data = repr(data)[:-3][1:]
-    while "  " in data:
-        data = data.replace("  ", " ")
-    with open("chatrooms/{0}.dat".format(category), "a") as myfile:
-        myfile.write(data + "\n")
+current_time = 0
+
+while True:
+    current_stat = os.stat("input.txt")[8]
+    if current_stat > current_time:
+        current_time = current_stat
+        print("Change Detect")
+        with open("input.txt") as fp:
+            line = fp.readline()
+            category = "Unknown"
+            data = ""
+            cnt = 1
+            while line:
+                if  cnt == 1:
+                    category = line.lower().replace('\n', '')
+                else:
+                    data += line + "\n" 
+                line = fp.readline()
+                cnt += 1
+            data = repr(data)[:-3][1:]
+            while "  " in data:
+                data = data.replace("  ", " ")
+            with open("chatrooms/{0}.dat".format(category), "a") as myfile:
+                myfile.write(data + "\n")
